e84db1cbe044c0d54193ade12d46da6c77a0564c
SeanKim <sean.kim84@gmail.com>
2018-07-20 10:16:07 +0700

    Create Hash and CreateNewBlock

--- src/blockchain.js
+++ src/blockchain.js
@@ -26,9 +26,27 @@ const getLastBlock = () => blockchain[blockchain.length - 1];
 
 const getTimeStamp = () => new Date().getTime() / 1000 ;
 
+const createHash = (index, previousHash, timestamp, data ) => {
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+}
+
 const createNewBlock = data => {
     const previousBlock = getLastBlock(); 
     const newBlockIndex = previousBlock.index + 1;
     const newTimeStamp = getTimeStamp();
 
-}
+    const newHash = createHash(
+        newBlockIndex,
+        previousBlock.hash, 
+        newTimeStamp, 
+        data
+    );
+    const newBlock = new Block(
+        newBlockIndex,
+        newHash,
+        previousBlock.hash,
+        newTimeStamp,
+        data
+    );
+    return newBlock;
+};
