a6639ea1a5fec99ed9ad26e4577502a0556f4323
SeanKim <sean.kim84@gmail.com>
2018-08-03 23:36:56 +0700

    P2P Error handling

--- src/p2p.js
+++ src/p2p.js
@@ -14,6 +14,7 @@ const startP2PServer = server => {
 
 const initSocketConnection = socket => {
   sockets.push(socket);
+  handleSocketError(socket);
   socket.on("message", (data) =>   { // message를 받으면 data를 console.log
     console.log(data);
   });
@@ -21,6 +22,15 @@ const initSocketConnection = socket => {
     socket.send("welcome");
   }, 5000);
 };
+
+const handleSocketError = ws => {
+  const closeSocketConnection = ws => {
+    ws.close()
+    sockets.splice(sockets.indexOf(ws), 1);
+  };
+  ws.on("close", () => closeSocketConnection(ws));
+  ws.on("error", () => closeSocketConnection(ws));
+}
 
 const connectToPeers = newPeer => {
   const ws = new WebSockets(newPeer);
